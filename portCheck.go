b6c47e6d01983b53894f6f8ddbaa708e7860451e
Samuel Moran <srmoran93@gmail.com>
2022-12-03 23:15:10 -0500

    added multichannel communication and concurrency

--- portCheck.go
+++ portCheck.go
@@ -2,7 +2,7 @@
 // Date: 12/3/2022
 // Description:
 /*
-	This Go program runs a TCP port scanner. As a refresher, a TCP connection involves a three-way handshake (syn, syn-ack, ack) to establish a connection between a client and server. The connection can result in one of three ways: 
+	This Go program runs a TCP port scanner. As a refresher, a TCP connection involves a three-way handshake (client sends "syn" packet, server responds with "syn-ack" packet, client responds with "ack" packet) to establish a connection between a client and server. The connection can result in one of three ways: 
 
 	1) Open Port: The three-way handshake results in an "ack" acknowledgment returned to the target server.
 
@@ -23,48 +23,81 @@ package main
 import(
 	"fmt"
 	"net"
-	"sync"
+	"sort"
 )
 
 // Edit this to view a smaller or larger port range
 const PORT_RANGE = 1024
 
 
+// The worker will take a channel to receive work from and the WaitGroup will track when that unit of work has been completed by that worker
+func worker(ports, results chan int){
+
+	for p :=range ports {
+		address := fmt.Sprintf("scanme.nmap.org:%d", p)
+		conn, err := net.Dial("tcp", address)
+		if err != nil {
+			results <- 0
+			continue
+		}
+		conn.Close()
+		results <- p
+	
+	}
+
+}
+
+
+
+
 func main() {
+
+
+	// Create channel
+	// The channel will provide work to a pool of 100 workers.
+	ports := make(chan int, 100)
+	results := make(chan int)
+	var openports []int
 
 	// The WaitGroup is a struct that will let us implement concurrency in a thread-safe way. 
 	// Add() will increase the internal counter by one for each port being scanned at the moment.
 	// Done() will decrement the counter by one once the port has gone through its scan.
 	// Wait() will block execution of the goroutine in which it's called until the internal counter reaches zero.
 	// This will ensure that our goroutine waits until the connection actually takes place so that it doesn't complete and exit once the loop finishes.
-	var wg sync.WaitGroup
+	// var wg sync.WaitGroup
 
-	// Iterate through port range
-	for i := 1; i <= PORT_RANGE; i++ {
 
-		wg.Add(1)
+	for i := 0; i <= cap(ports); i++ {
 
-		go func(j int){
+		go worker(ports, results)
 
-			defer wg.Done()
+	}
 
-			// Format address for Go Dial function
-			address := fmt.Sprintf("scanme.nmap.org:%d", j)
-			conn, err := net.Dial("tcp", address)
+	go func() {
 
-			// In the event port is closed or filtered through firewall, have the program continue
-			if err != nil {
-				return
-			}
+		// Iterate through port range
+		for i := 1; i <= PORT_RANGE; i++ {
 
-			// Close connection
-			conn.Close()
-			fmt.Printf("%d open\n", j)
+			ports <- i
 
-		}(i)
+		}
+	}()
 
-		wg.Wait()
+	for i := 0; i < PORT_RANGE; i++ {
 
+		port := <-results
+
+		if port != 0{
+			openports = append(openports, port)
+		}
+
+	}
+	close(ports)
+	close(results)
+	sort.Ints(openports)
+	
+	for _, port := range openports {
+		fmt.Println("%d open\n", port)
 	}
 
 }
